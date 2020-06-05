2d576e28bb05e976b707c2d1522d88dc5f7ce6b5
Hyago Ribeiro <hyagogabriel_r@hotmail.com>
2020-06-05 20:19:50 -0300

    Tentativa de fazer o desafio novamente

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -12,44 +12,73 @@
  */
 
 
-const rodadaInicial = 2
 const venceu = 21
 console.log("Bem vindo ao jogo de Blackjack!")
 let pergunta = confirm("Quer iniciar uma nova partida?")
-let computador = [compararCarta(), comprarCarta()]
-let usuario = [compararCarta(), comprarCarta()]
 let continuar = true
 let total1 = 0
 let total2 = 0
 
- if(pergunta === true){
+
+//while porque precisa repetir e dar novos valores acaso o usuário queira jogar novamente
+ while(pergunta){
+//declaração iniciao de cada jogador
+   let computador = [comprarCarta(), comprarCarta()]
+   let usuario = [comprarCarta(), comprarCarta()]
+
+   console.log(usuario[0].valor, usuario[1].valor)
+//soma do usuario
+   total1 =  usuario[0].valor + usuario[1].valor
+
+   console.log(total1)
+
+   pergunta = confirm("Quer comprar mais uma carta?")
+
+   //esse while é para fazer o pedir cartas novas conforme o usuário esteja insatisfeito
+      while(pergunta){
+
+         if(total1 <= venceu){
+            ultimoUsuario = usuario[usuario.length -1]
+            usuario.push(comprarCarta())
+
+            total1 = total1 + ultimoUsuario.valor
+            
+            console.log(usuario)
+            console.log(total1)
+         
+            pergunta = confirm("Quer comprar mais uma carta?")
+
+         }
+
+      }
+
     //usuário
       //usar while no nossoJogo
       //focar na funcionalidade para que o usuário possa jogar o quanto desejar
       //o jogo só acaba quando o usuário deseja sair do jogo
       //
 
-         while(pergunta){
+         // while(pergunta){
              
-            if(total1 <= venceu){
+         //    if(total1 <= venceu){
 
-               computador.push(comprarCarta())
+         //       computador.push(comprarCarta())
 
-               usuario.push(comprarCarta())
+         //       usuario.push(comprarCarta())
 
-               total1 = total1 + computador[i].valor
+         //       total1 = total1 + computador[i].valor
 
-               total2 = total2 + usuario[i].valor
+         //       total2 = total2 + usuario[i].valor
                
-              }
+         //      }
               
-         }
+         // }
    
-         console.log(computador[0].texto + " " + computador[1].texto)
-         console.log(usuario[0].texto)
-         console.log(usuario[1].texto)
-         console.log(total1)
-         console.log(total2)
+         // console.log(computador[0].texto + " " + computador[1].texto)
+         // console.log(usuario[0].texto)
+         // console.log(usuario[1].texto)
+         // console.log(total1)
+         // console.log(total2)
          
          
          // computador.push(comprarCarta())
