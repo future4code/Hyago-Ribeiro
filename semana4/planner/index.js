0887b200d94f7ea16407a571d8692da9009201b1
Hyago Ribeiro <hyagogabriel_r@hotmail.com>
2020-06-09 16:00:07 -0300

    exercício planner

---
+++ semana4/planner/index.js
@@ -0,0 +1,86 @@
+function novaTarefa() {
+
+    let meuInput = document.getElementById("addTask")
+    let tarefa = meuInput.value
+    
+    let diasDaSemana = document.getElementById("diaSemana")
+
+
+    switch(diasDaSemana.value){
+        
+            case 'segunda' :
+
+                let elemento = document.getElementById("segunda");
+
+                elemento.innerHTML += `<li> ${tarefa} </li>`;
+
+                meuInput.value = " ";
+                break;
+
+            case 'terca' :
+
+                let elemento2 = document.getElementById("terca");
+
+                elemento2.innerHTML += `<li> ${tarefa} </li>`;
+
+                meuInput.value = " ";
+                break;
+
+            case 'quarta' :
+
+                let elemento4 = document.getElementById("quarta");
+
+                elemento4.innerHTML += `<li> ${tarefa} </li>`;
+
+                meuInput.value = " ";
+                break;
+
+            case 'quinta' :
+
+                let elemento5 = document.getElementById("quinta");
+
+                elemento5.innerHTML += `<li> ${tarefa} </li>`;
+
+                meuInput.value = " ";
+                break;
+
+            case 'sexta' :
+
+                let elemento6 = document.getElementById("sexta");
+
+                elemento6.innerHTML += `<li> ${tarefa} </li>`;
+
+                meuInput.value = " ";
+                break;
+
+            case 'sabado' :
+
+                let elemento7 = document.getElementById("sabado");
+
+                elemento7.innerHTML += `<li> ${tarefa} </li>`;
+
+                meuInput.value = " ";
+                break;
+
+            case 'domingo' :
+
+                let elemento8 = document.getElementById("domingo");
+
+                elemento8.innerHTML += `<li> ${tarefa} </li>`;
+
+                meuInput.value = " ";
+                break;
+        
+       
+
+            
+    
+
+    }
+    
+
+    // console.log(tarefa)
+    
+
+    // console.log(diasDaSemana[1].value)
+}
