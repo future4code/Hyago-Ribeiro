007061cea5be72b08a816c3ec408d279ac08322c
Hyago Ribeiro <hyagogabriel_r@hotmail.com>
2020-06-09 17:30:21 -0300

    ajustes. Desafio 1

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -5,82 +5,79 @@ function novaTarefa() {
     
     let diasDaSemana = document.getElementById("diaSemana")
 
-
-    switch(diasDaSemana.value){
-        
-            case 'segunda' :
-
-                let elemento = document.getElementById("segunda");
-
-                elemento.innerHTML += `<li> ${tarefa} </li>`;
-
-                meuInput.value = " ";
-                break;
-
-            case 'terca' :
-
-                let elemento2 = document.getElementById("terca");
-
-                elemento2.innerHTML += `<li> ${tarefa} </li>`;
-
-                meuInput.value = " ";
-                break;
-
-            case 'quarta' :
-
-                let elemento4 = document.getElementById("quarta");
-
-                elemento4.innerHTML += `<li> ${tarefa} </li>`;
-
-                meuInput.value = " ";
-                break;
-
-            case 'quinta' :
-
-                let elemento5 = document.getElementById("quinta");
-
-                elemento5.innerHTML += `<li> ${tarefa} </li>`;
-
-                meuInput.value = " ";
-                break;
-
-            case 'sexta' :
-
-                let elemento6 = document.getElementById("sexta");
-
-                elemento6.innerHTML += `<li> ${tarefa} </li>`;
-
-                meuInput.value = " ";
-                break;
-
-            case 'sabado' :
-
-                let elemento7 = document.getElementById("sabado");
-
-                elemento7.innerHTML += `<li> ${tarefa} </li>`;
-
-                meuInput.value = " ";
-                break;
-
-            case 'domingo' :
-
-                let elemento8 = document.getElementById("domingo");
-
-                elemento8.innerHTML += `<li> ${tarefa} </li>`;
-
-                meuInput.value = " ";
-                break;
-        
-       
-
-            
     
 
+    if(tarefa === ""){
+        alert(`Não é possível adicionar uma lista vazia. Adicione algo.`)
+    
+
+    } else {  switch(diasDaSemana.value){
+            
+                case 'segunda' :
+
+                    let elemento = document.getElementById("segunda");
+
+                    elemento.innerHTML += `<li> ${tarefa} </li>`;
+
+                    meuInput.value = " ";
+                    break;
+
+                case 'terca' :
+                    
+                    let elemento2 = document.getElementById("terca");
+
+                    elemento2.innerHTML += `<li> ${tarefa} </li>`;
+
+                    meuInput.value = " ";
+                    break;
+
+                case 'quarta' :
+
+                    let elemento4 = document.getElementById("quarta");
+
+                    elemento4.innerHTML += `<li> ${tarefa} </li>`;
+
+                    meuInput.value = " ";
+                    break;
+
+                case 'quinta' :
+
+                    let elemento5 = document.getElementById("quinta");
+
+                    elemento5.innerHTML += `<li> ${tarefa} </li>`;
+
+                    meuInput.value = " ";
+                    break;
+
+                case 'sexta' :
+
+                    let elemento6 = document.getElementById("sexta");
+
+                    elemento6.innerHTML += `<li> ${tarefa} </li>`;
+
+                    meuInput.value = " ";
+                    break;
+
+                case 'sabado' :
+
+                    let elemento7 = document.getElementById("sabado");
+
+                    elemento7.innerHTML += `<li> ${tarefa} </li>`;
+
+                    meuInput.value = " ";
+                    break;
+
+                case 'domingo' :
+
+                    let elemento8 = document.getElementById("domingo");
+
+                    elemento8.innerHTML += `<li> ${tarefa} </li>`;
+
+                    meuInput.value = " ";
+                    break;
+
+        }
+    
     }
     
-
-    // console.log(tarefa)
-    
-
-    // console.log(diasDaSemana[1].value)
 }
