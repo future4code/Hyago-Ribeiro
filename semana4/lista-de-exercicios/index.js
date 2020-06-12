cf360e9fcc2936dcd535a558bc0120315f41dac8
Hyago Ribeiro <hyagogabriel_r@hotmail.com>
2020-06-12 11:12:56 -0300

    Exercícios de interpretação 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -2,7 +2,14 @@
 1.
 No código tem uma função responsável pela conversão de valores. Precisa ser passado um parametro dentro da função, que será utilizado no decorrer dela. O valor da cotação é informado pelo usuário na execução da função. Fora dela é atribuido a uma const a chamada da função e o valor a ser convertido(100). No console log essa variável é chamada e por conter a execução da função, a mesma é executada. O resultado vai depender do valor inserito no prompt, se for 2, o resultado seja R$ 200.
 
-
+2.
+Uma função para calcular investimento, são passados dois parametros, o tipo e o valor. 
+No inicio é declarada um let para armazenar o resultado. 
+o Switch é utilizado como forma se identificar o tipo de investimento. Se o parametro passado na função for compatível com algum dos Case, o codigo será executado. 
+Dentro do case, o código vai atribuir a variável valorAposInvestimento o valor do parametro * a proporção do investimento.
+Se não for passado nenhuma das opções existentes no parametro, vai cair em default e exibe um alert que o tipo é incorreto.
+No final da função ela retorna a variável ValorAposInvestimento com o novo valor atribuido.
+Depois é declarada duas const que recebem o chamado da função investeDinheiro com dois parametros cada uma. As duas variáveis que foram declaradas, são chamas em dois console.log diferente. A primeira chamada novoMontante vai dar um retorno de 165. Já a segunda vai exibir um alert, pois não encaixa em nenhum tipo de investimento apresentado.
 
 
 */
