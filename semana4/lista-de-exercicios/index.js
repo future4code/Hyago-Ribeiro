d0d9085784b033cfbc473df1b1c499d158aa001d
Hyago Ribeiro <hyagogabriel_r@hotmail.com>
2020-06-12 11:12:26 -0300

    exercícios de interpretação 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,8 @@
+/* Exercícios de interpretação de código
+1.
+No código tem uma função responsável pela conversão de valores. Precisa ser passado um parametro dentro da função, que será utilizado no decorrer dela. O valor da cotação é informado pelo usuário na execução da função. Fora dela é atribuido a uma const a chamada da função e o valor a ser convertido(100). No console log essa variável é chamada e por conter a execução da função, a mesma é executada. O resultado vai depender do valor inserito no prompt, se for 2, o resultado seja R$ 200.
+
+
+
+
+*/
